bc3b550515bc02790baed4e2034a2920717fdb10
junogw <100618991+junogw@users.noreply.github.com>
2023-08-21 10:47:49 -0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,183 @@
+terraform {
+  required_providers {
+    # for the API connections to Google Workspace
+    google = {
+      version = ">= 3.74, <= 5.0"
+    }
+  }
+
+  # we recommend you use a secure location for your Terraform state (such as S3 bucket), as it
+  # may contain sensitive values (such as API keys) depending on which data sources you configure.
+  #
+  # local may be safe for production-use IFF you are executing Terraform from a secure location
+  #
+  # Please review and seek guidance from your Security team if in doubt.
+  backend "local" {
+  }
+
+  # example remote backend (this GCS bucket must already be provisioned, and GCP user executing
+  # terraform must be able to read/write to it)
+  #  backend "gcs" {
+  #    bucket  = "tf-state-prod"
+  #    prefix  = "proxy/terraform-state"
+  #  }
+}
+
+provider "google" {
+  impersonate_service_account = var.gcp_terraform_sa_account_email
+}
+
+locals {
+  host_platform_id = "GCP"
+}
+
+# TODO: this has 5 remote modules; combine some?
+#  eg, worklytics-connectors + gcp-host + worklytics-psoxy-connection-generic into a single
+#     gcp-host-for-worklytics? poor TF style, but simplifies root module?
+
+# in effect, these are for sources for which authentication/authorization cannot (or need not)
+# be provisioned via Terraform, so doesn't add any dependencies
+# call this 'generic_source_connectors'?
+module "worklytics_connectors" {
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.34"
+
+
+  enabled_connectors            = var.enabled_connectors
+  jira_cloud_id                 = var.jira_cloud_id
+  jira_server_url               = var.jira_server_url
+  jira_example_issue_id         = var.jira_example_issue_id
+  salesforce_domain             = var.salesforce_domain
+  github_installation_id        = var.github_installation_id
+  github_organization           = var.github_organization
+  github_example_repository     = var.github_example_repository
+  salesforce_example_account_id = var.salesforce_example_account_id
+}
+
+# sources which require additional dependencies are split into distinct Terraform files, following
+# the naming convention of `{source-identifier}.tf`, eg `msft-365.tf`
+# lines below merge results of those files back into single maps of sources
+locals {
+  api_connectors = merge(
+    module.worklytics_connectors.enabled_api_connectors,
+    module.worklytics_connectors_google_workspace.enabled_api_connectors,
+    # local.msft_api_connectors_with_auth,
+    {}
+  )
+
+  source_authorization_todos = concat(
+    module.worklytics_connectors.todos,
+    module.worklytics_connectors_google_workspace.todos,
+    # module.worklytics_connectors_msft_365.todos,
+    []
+  )
+
+  max_auth_todo_step = max(
+    module.worklytics_connectors.next_todo_step,
+    module.worklytics_connectors_google_workspace.next_todo_step,
+    # module.worklytics_connectors_msft_365.next_todo_step,
+    0
+  )
+}
+
+locals {
+  bulk_connectors = merge(
+    module.worklytics_connectors.enabled_bulk_connectors,
+    var.custom_bulk_connectors,
+  )
+}
+
+module "psoxy" {
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/gcp-host?ref=v0.4.34"
+
+  gcp_project_id                  = var.gcp_project_id
+  environment_name                = var.environment_name
+  config_parameter_prefix         = var.config_parameter_prefix
+  default_labels                  = var.default_labels
+  worklytics_sa_emails            = var.worklytics_sa_emails
+  psoxy_base_dir                  = var.psoxy_base_dir
+  deployment_bundle               = var.deployment_bundle
+  force_bundle                    = var.force_bundle
+  install_test_tool               = var.install_test_tool
+  gcp_region                      = var.gcp_region
+  replica_regions                 = coalesce(var.replica_regions, var.gcp_secret_replica_locations)
+  api_connectors                  = local.api_connectors
+  bulk_connectors                 = local.bulk_connectors
+  non_production_connectors       = var.non_production_connectors
+  custom_api_connector_rules      = var.custom_api_connector_rules
+  general_environment_variables   = var.general_environment_variables
+  pseudonymize_app_ids            = var.pseudonymize_app_ids
+  bulk_input_expiration_days      = var.bulk_input_expiration_days
+  bulk_sanitized_expiration_days  = var.bulk_sanitized_expiration_days
+  custom_bulk_connector_rules     = var.custom_bulk_connector_rules
+  custom_bulk_connector_arguments = var.custom_bulk_connector_arguments
+  lookup_tables                   = var.lookup_tables
+  custom_artifacts_bucket_name    = var.custom_artifacts_bucket_name
+  todos_as_local_files            = var.todos_as_local_files
+  todo_step                       = local.max_auth_todo_step
+}
+
+locals {
+  all_connectors = merge(local.api_connectors, local.bulk_connectors)
+  all_instances  = merge(module.psoxy.bulk_connector_instances, module.psoxy.api_connector_instances)
+}
+
+module "connection_in_worklytics" {
+  for_each = local.all_instances
+
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-generic?ref=v0.4.34"
+
+  psoxy_host_platform_id = local.host_platform_id
+  psoxy_instance_id      = each.key
+  worklytics_host        = var.worklytics_host
+  connector_id           = try(local.all_connectors[each.key].worklytics_connector_id, "")
+  display_name           = try(local.all_connectors[each.key].worklytics_connector_name, "${local.all_connectors[each.key].display_name} via Psoxy")
+  todo_step              = module.psoxy.next_todo_step
+
+  settings_to_provide = merge(
+    # Source API case
+    try({
+      "Psoxy Base URL" = each.value.endpoint_url
+    }, {}),
+    # Source Bucket (file) case
+    try({
+      "Bucket Name" = each.value.sanitized_bucket
+    }, {}),
+  try(each.value.settings_to_provide, {}))
+}
+
+output "path_to_deployment_jar" {
+  description = "Path to the package to deploy (JAR)."
+  value       = module.psoxy.path_to_deployment_jar
+}
+
+output "todos_1" {
+  description = "List of todo steps to complete 1st, in markdown format."
+  value = var.todos_as_outputs ? join("\n",
+    concat(
+      module.worklytics_connectors.todos,
+      module.worklytics_connectors_google_workspace.todos
+  )) : null
+}
+
+output "todos_2" {
+  description = "List of todo steps to complete 2nd, in markdown format."
+  value       = var.todos_as_outputs ? join("\n", module.psoxy.todos) : null
+}
+
+output "todos_3" {
+  description = "List of todo steps to complete 3rd, in markdown format."
+  value       = var.todos_as_outputs ? join("\n", values(module.connection_in_worklytics)[*].todo) : null
+}
+
+moved {
+  from = module.psoxy.module.secrets["jira-cloud"].google_secret_manager_secret.secret["JIRA_CLOUD_REFRESH_TOKEN"]
+  to   = module.psoxy.module.secrets["jira-cloud"].google_secret_manager_secret.secret["REFRESH_TOKEN"]
+}
+moved {
+  from = module.psoxy.module.secrets["jira-cloud"].google_secret_manager_secret_version.version["JIRA_CLOUD_REFRESH_TOKEN"]
+  to   = module.psoxy.module.secrets["jira-cloud"].google_secret_manager_secret_version.version["REFRESH_TOKEN"]
+}
+moved {
+  from = module.psoxy.module.api_connector["jira-cloud"].google_secret_manager_secret_iam_member.grant_sa_accessor_on_secret["JIRA_CLOUD_REFRESH_TOKEN"]
+  to   = module.psoxy.module.api_connector["jira-cloud"].google_secret_manager_secret_iam_member.grant_sa_accessor_on_secret["REFRESH_TOKEN"]
+}
---
+++ variables.tf
@@ -0,0 +1,287 @@
+variable "gcp_project_id" {
+  type        = string
+  description = "id of GCP project that will host psoxy instance"
+}
+
+variable "gcp_terraform_sa_account_email" {
+  type        = string
+  description = "Email of GCP service account that will be used to provision GCP resources. Leave 'null' to use application default for you environment."
+  default     = null
+
+  validation {
+    condition     = var.gcp_terraform_sa_account_email == null || can(regex(".*@.*\\.iam\\.gserviceaccount\\.com$", var.gcp_terraform_sa_account_email))
+    error_message = "The gcp_terraform_sa_account_email value should be a valid GCP service account email address."
+  }
+}
+
+variable "environment_name" {
+  type        = string
+  description = "Qualifier to append to names/ids of resources for psoxy. If not empty, A-Za-z0-9 or - characters only. Max length 10. Useful to distinguish between deployments into same GCP project."
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[A-z0-9\\-]{0,20}$", var.environment_name))
+    error_message = "The environment_id must be 0-20 chars of [A-z0-9\\-] only."
+  }
+}
+
+variable "config_parameter_prefix" {
+  type        = string
+  description = "A prefix to give to all config parameters (GCP Secret Manager Secrets) created/consumed by this module. If omitted, and `environment_id` provided, that will be used."
+  default     = ""
+}
+
+variable "default_labels" {
+  type        = map(string)
+  description = "Labels to apply to all resources created by this configuration. Intended to be analogous to AWS providers `default_tags`."
+  default     = {}
+
+  validation {
+    condition     = alltrue([for k, v in var.default_labels : can(regex("^[a-z][a-z0-9-_]{0,62}$", k))])
+    error_message = "GCP label keys must start with a lowercase letter, can contain lowercase letters, numbers, underscores and dashes only and must be no longer than 63 characters."
+  }
+
+  validation {
+    condition     = alltrue([for k, v in var.default_labels : can(regex("^[a-z0-9-_]{0,63}$", v))])
+    error_message = "GCP label values must contain only lowercase letters, numbers, underscores and dashes only and be no longer than 63 characters."
+  }
+
+  validation {
+    condition     = length(var.default_labels) <= 64
+    error_message = "GCP resources cannot have more than 64 labels."
+  }
+}
+
+variable "worklytics_host" {
+  type        = string
+  description = "host of worklytics instance where tenant resides. (e.g. intl.worklytics.co for prod; but may differ for dev/staging)"
+  default     = "intl.worklytics.co"
+}
+
+variable "worklytics_sa_emails" {
+  type        = list(string)
+  description = "service accounts for your organization's Worklytics instances (list supported for test/dev scenarios)"
+}
+
+variable "psoxy_base_dir" {
+  type        = string
+  description = "the path where your psoxy repo resides"
+
+  validation {
+    condition     = can(regex(".*\\/$", var.psoxy_base_dir))
+    error_message = "The psoxy_base_dir value should end with a slash."
+  }
+  validation {
+    condition     = can(regex("^[^~].*$", var.psoxy_base_dir))
+    error_message = "The psoxy_base_dir value should be absolute path (not start with ~)."
+  }
+}
+
+variable "deployment_bundle" {
+  type        = string
+  description = "path to deployment bundle to use (if not provided, will build one). Can be GCS url, eg 'gs://artifacts-bucket/psoxy-0.4.28.zip'."
+  default     = null
+
+  validation {
+    condition     = var.deployment_bundle == null || var.deployment_bundle != ""
+    error_message = "`deployment_bundle`, if non-null, must be non-empty string."
+  }
+}
+
+variable "force_bundle" {
+  type        = bool
+  description = "whether to force build of deployment bundle, even if it already exists"
+  default     = false
+}
+
+variable "install_test_tool" {
+  type        = bool
+  description = "whether to install the test tool (can be 'false' if Terraform not running from a machine where you intend to run tests of your Psoxy deployment)"
+  default     = true
+}
+
+variable "general_environment_variables" {
+  type        = map(string)
+  description = "environment variables to add for all connectors"
+  default     = {}
+}
+
+variable "pseudonymize_app_ids" {
+  type        = string
+  description = "if set, will set value of PSEUDONYMIZE_APP_IDS environment variable to this value for all sources"
+  default     = true
+}
+
+variable "gcp_region" {
+  type        = string
+  description = "Region in which to provision GCP resources, if applicable"
+  default     = "us-central1"
+}
+
+variable "replica_regions" {
+  type        = list(string)
+  description = "DEPRECATED; use `gcp_secret_replica_locations`. List of locations to which to replicate secrets. See https://cloud.google.com/secret-manager/docs/locations"
+  default     = null
+}
+
+variable "gcp_secret_replica_locations" {
+  type        = list(string)
+  description = "List of locations to which to replicate GCP Secret Manager secrets. See https://cloud.google.com/secret-manager/docs/locations"
+  default = [
+    "us-central1",
+    "us-west1",
+  ]
+
+  validation {
+    condition     = length(var.gcp_secret_replica_locations) > 0
+    error_message = "`gcp_secret_replica_locations` must be non-empty list."
+  }
+}
+
+variable "custom_artifacts_bucket_name" {
+  type        = string
+  description = "name of bucket to use for custom artifacts, if you want something other than default. Ignored if you pass gcs url for `deployment_bundle`."
+  default     = null
+}
+
+variable "enabled_connectors" {
+  type        = list(string)
+  description = "list of ids of connectors to enabled; see modules/worklytics-connector-specs"
+}
+
+variable "non_production_connectors" {
+  type        = list(string)
+  description = "connector ids in this list will be in development mode (not for production use"
+  default     = []
+}
+
+variable "bulk_input_expiration_days" {
+  type        = number
+  description = "Number of days after which objects in the bucket will expire"
+  default     = 30
+}
+
+variable "bulk_sanitized_expiration_days" {
+  type        = number
+  description = "Number of days after which objects in the bucket will expire"
+  default     = 1805 # 5 years; intent is 'forever', but some upperbound in case bucket is forgotten
+}
+
+variable "custom_api_connector_rules" {
+  type        = map(string)
+  description = "map of connector id --> YAML file with custom rules"
+  default     = {}
+}
+
+variable "custom_bulk_connectors" {
+  type = map(object({
+    source_kind           = string
+    input_bucket_name     = optional(string) # allow override of default bucket name
+    sanitized_bucket_name = optional(string) # allow override of default bucket name
+    rules = object({
+      pseudonymFormat       = optional(string, "URL_SAFE_TOKEN")
+      columnsToRedact       = optional(list(string)) # columns to remove from CSV
+      columnsToInclude      = optional(list(string)) # if you prefer to include only an explicit list of columns, rather than redacting those you don't want
+      columnsToPseudonymize = optional(list(string)) # columns to pseudonymize
+      columnsToDuplicate    = optional(map(string))  # columns to create copy of; name --> new name
+      columnsToRename       = optional(map(string))  # columns to rename: original name --> new name; renames applied BEFORE pseudonymization
+    })
+    settings_to_provide = optional(map(string), {})
+  }))
+  description = "specs of custom bulk connectors to create"
+
+  default = {
+    #    "custom-survey" = {
+    #      source_kind = "survey"
+    #      rules       = {
+    #        columnsToRedact       = []
+    #        columnsToPseudonymize = [
+    #          "employee_id", # primary key
+    #          # "employee_email", # if exists
+    #        ]
+    #      }
+    #    }
+  }
+}
+
+variable "custom_bulk_connector_rules" {
+  type = map(object({
+    pseudonymFormat       = optional(string, "URL_SAFE_TOKEN")
+    columnsToRedact       = optional(list(string)) # columns to remove from CSV
+    columnsToInclude      = optional(list(string)) # if you prefer to include only an explicit list of columns, rather than redacting those you don't want
+    columnsToPseudonymize = optional(list(string)) # columns to pseudonymize
+    columnsToDuplicate    = optional(map(string))  # columns to create copy of; name --> new name
+    columnsToRename       = optional(map(string))  # columns to rename: original name --> new name; renames applied BEFORE pseudonymization
+  }))
+
+  description = "map of connector id --> rules object"
+  default = {
+    # hris = {
+    #   columnsToRedact       = []
+    #   columnsToPseudonymize = [
+    #     "EMPLOYEE_ID",
+    #     "EMPLOYEE_EMAIL",
+    #     "MANAGER_ID",
+    #     "MANAGER_EMAIL"
+    #  ]
+    # columnsToRename = {
+    #   # original --> new
+    #   "workday_id" = "employee_id"
+    # }
+    # columnsToInclude = [
+    # ]
+  }
+}
+
+variable "custom_bulk_connector_arguments" {
+  type = map(object({
+    available_memory_mb = optional(number)
+    # what else to add here?
+  }))
+
+  description = "map of connector id --> arguments object, to override defaults for bulk connector instances"
+  default     = {}
+}
+
+# build lookup tables to JOIN data you receive back from Worklytics with your original data.
+#   - `join_key_column` should be the column you expect to JOIN on, usually 'employee_id'
+#   - `columns_to_include` is an optional a list of columns to include in the lookup table,
+#                       e.g. if the data you're exporting TO worklytics contains more columns than
+#                       you want to have in the lookup table, you can limit to an explicit list
+#   - `sanitized_accessor_names` is an optional list of GCP principals, by email with qualifier, eg:
+#                       `user:alice@worklytics`, `group:analysts@worklytics.co`, or
+#                        `serviceAccount:sa@worklytics.google-service-accounts.com`
+variable "lookup_tables" {
+  type = map(object({
+    source_connector_id           = string
+    join_key_column               = string
+    columns_to_include            = optional(list(string))
+    sanitized_accessor_principals = optional(list(string))
+    expiration_days               = optional(number)
+    output_bucket_name            = optional(string) # allow override of default bucket name
+  }))
+  description = "Lookup tables to build from same source input as another connector, output to a distinct bucket. The original `join_key_column` will be preserved, "
+
+  default = {
+    #  "lookup-hris" = {
+    #      source_connector_id = "hris",
+    #      join_key_column = "employee_id",
+    #      columns_to_include = null
+    #      sanitized_accessor_principals = [
+    #        # ADD LIST OF GCP PRINCIPALS HERE
+    #      ],
+    #  }
+  }
+}
+
+variable "todos_as_outputs" {
+  type        = bool
+  description = "whether to render TODOs as outputs (useful if you're using Terraform Cloud/Enterprise, or somewhere else where the filesystem is not readily accessible to you)"
+  default     = false
+}
+
+variable "todos_as_local_files" {
+  type        = bool
+  description = "whether to render TODOs as flat files"
+  default     = true
+}
